9db999dc98b2d82e6498a4802c1fddd93e3c6b49
Gourav65 <140390122+Gourav65@users.noreply.github.com>
2024-06-11 19:09:16 +0530

    correct path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<Home/> } />
+        <Route path='/' element={<Home/> } />
         <Route path='/about' element={<About/> } />
         <Route path='/sign-in' element={<Signin/> } />
         <Route path='/sign-up' element={<Signup/> } />
