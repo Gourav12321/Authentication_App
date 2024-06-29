f32b82a3a2d0dd04ca290214f81e03f066dea29c
Gourav65 <140390122+Gourav65@users.noreply.github.com>
2024-06-29 22:04:13 +0530

    change

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // Adjust the limit to your preference, e.g., 2000 KB
+  },
   server: {
     proxy: {
       '/api': {
---
+++ client/webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js
+module.exports = {
+    // Other configurations...
+    performance: {
+      maxAssetSize: 2000000, // Adjust the limit to your preference, e.g., 2000 KB
+      maxEntrypointSize: 2000000, // Adjust the limit to your preference, e.g., 2000 KB
+    },
+  };
+  
